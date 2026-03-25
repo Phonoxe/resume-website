8d701a7b0b6ca2449e340c70dba20ef64d0075ac
Leopold Stiffel <leopold.stiffel@epfedu.fr>
2026-03-25 17:08:25 +0100

    Some classes created, still missing some others, no route created

--- main.py
+++ main.py
@@ -1,1 +1,39 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
+
+app = FastAPI()
+
+
+class Interest(BaseModel):
+    name: str
+    description: str
+
+
+class PersonnalInfo(BaseModel):
+    name: str
+    birth_date: str
+    home: str
+    coordinate: dict
+    interest: list[Interest]
+
+
+class LanguageSkill(BaseModel):
+    language: str
+    level: str
+
+
+class Experience(BaseModel):
+    type: str
+    description: str
+    starting_date: str
+    ending_date: str
+
+
+class Formation(BaseModel):
+    ref: str
+    description: __repr_str__
+
+
+@app.get("/")
+def read_root():
+    return {"Hello": "World"}
